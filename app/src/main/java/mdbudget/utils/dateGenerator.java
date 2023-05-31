36ac67efe37f5c72a11dc148d4f99719168a31b6
NitoCoding <ilham.mks003@gmail.com>
2023-06-01 01:39:06 +0800

    fix: naming DateGenerator util

--- app/src/main/java/mdbudget/utils/dateGenerator.java
+++ app/src/main/java/mdbudget/utils/dateGenerator.java
@@ -2,7 +2,7 @@ package mdbudget.utils;
 
 import java.util.Date;
 
-public class dateGenerator {
+public class DateGenerator {
     public static String getDateString(){
         Date date = new Date();
         String time = Long.toString(date.getTime());
