8987fa22a3b7d2955ec526c16640510e69911104
NitoCoding <ilham.mks003@gmail.com>
2023-06-01 01:41:31 +0800

    feat: make parent class Scene

---
+++ app/src/main/java/mdbudget/scenes/BaseScene.java
@@ -0,0 +1,11 @@
+package mdbudget.scenes;
+
+import javafx.stage.Stage;
+
+public class BaseScene {
+    protected Stage stage;
+
+    public BaseScene(Stage stage){
+        this.stage = stage;
+    }
+}
