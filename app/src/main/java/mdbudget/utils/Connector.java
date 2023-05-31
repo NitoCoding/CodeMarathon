650b446255192f339c008ea3436497ccba12eb64
NitoCoding <ilham.mks003@gmail.com>
2023-06-01 01:40:09 +0800

    fix: move connector to util

---
+++ app/src/main/java/mdbudget/utils/Connector.java
@@ -0,0 +1,19 @@
+package mdbudget.config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Connector {
+    private static final String URL=  "jdbc:sqlite:db/MDBudget.db";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
