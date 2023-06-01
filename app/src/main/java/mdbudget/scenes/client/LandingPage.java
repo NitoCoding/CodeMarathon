1b3e0a215d58f51b31cbfc1d657c7325f639f56b
NitoCoding <ilham.mks003@gmail.com>
2023-06-02 06:11:46 +0800

    style: LandingPage

--- app/src/main/java/mdbudget/scenes/client/LandingPage.java
+++ app/src/main/java/mdbudget/scenes/client/LandingPage.java
@@ -7,9 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import mdbudget.controllers.UserController;
 import mdbudget.interfaces.Showable;
@@ -27,36 +25,72 @@ public class LandingPage extends BaseScene implements Showable {
 
     @Override
     public void show() {
+        GridPane headerContainer = new GridPane();
+
         Label loginLabel = new Label("Login");
-        loginLabel.setStyle("fx-font-size: 20px;-fx-font-weight: bold;-fx-aligment: center");
+        loginLabel.setStyle(
+            "-fx-font-family: 'Jacques Francois';" +
+            "-fx-font-size: 40px;" +
+            "-fx-font-weight: 400;" +
+            "-fx-line-height: 53px;" +
+            "-fx-letter-spacing: 0em;" +
+            "-fx-text-alignment: center;" +
+            "-fx-text-fill: #385748;"
+            // "-fx-effect: dropshadow(gaussian, rgba(0,0,0,0.25), 0px, 4px, 4px);"
+        );
+
+        headerContainer.add(loginLabel,0,0);
+        headerContainer.setAlignment(Pos.TOP_CENTER);
+
+        GridPane logoContainer = new GridPane();
 
         ImageView brandLogo = new ImageView(ImageGenerator.generate("burger.png", logoWidth, logoHeight));
 
-        StackPane brandContainer = new StackPane(brandLogo);
+        logoContainer.add(brandLogo,0,0);
+        // StackPane brandContainer = new StackPane(brandLogo);
+        logoContainer.setAlignment(Pos.CENTER);
+        //
 
-        VBox usernameField = new VBox();
-        Label usernameLabel =  new Label("username : ");
+        GridPane formContainer = new GridPane();
+        formContainer.setAlignment(Pos.CENTER);
+        formContainer.setHgap(10);
+        formContainer.setVgap(12);
+
+        Label usernameLabel = new Label("Username :");
         TextField username = new TextField();
-        username.setStyle("-fx-max-width: 300px; -fx-max-height: 50px;");
-        usernameField.getChildren().addAll(usernameLabel,username);
-        
-        VBox passwordField = new VBox();
-        Label passwordLabel =  new Label("password : ");
+        // username.setMaxWidth(250);
+        username.setMinWidth(250);
+        username.setMinHeight(30);;
+
+        Label passwordLabel = new Label("Password :");
         PasswordField password = new PasswordField();
-        password.setStyle("-fx-max-width: 300px; -fx-max-height: 50px;");
-        passwordField.getChildren().addAll(passwordLabel,password);
+        password.setMinWidth(250);
+        password.setMinHeight(30);
 
-        // password.setStyle("-fx-pref-width: 200px; -fx-pref-height: 30px;");
+        formContainer.add(usernameLabel, 0, 0);
+        formContainer.add(username, 0, 1);
+        formContainer.add(passwordLabel, 0, 2);
+        formContainer.add(password, 0, 3);
 
-        VBox formContainer = new VBox();
-        formContainer.getChildren().addAll(usernameField,passwordField);
-        formContainer.setAlignment(Pos.TOP_CENTER);
-        formContainer.setSpacing(15);
+        GridPane buttonContainer = new GridPane();
 
         Button loginButton = new Button("Login");
+        loginButton.setStyle(
+            "-fx-background-color: #58585B;" +
+            "-fx-border-color: none;" +
+            "-fx-background-radius: 10;"+
+            "-fx-font-family: 'Jaldi';" +
+            "-fx-font-size: 15px;" +
+            "-fx-font-weight: 400;" +
+            "-fx-line-height: 34px;" +
+            "-fx-text-fill: white;"
+        );
+        loginButton.setMinWidth(115);
+        loginButton.setMinHeight(45);
         loginButton.setOnAction(action -> {
             String formUsername = username.getText();
             String formPassword = password.getText();
+            System.out.println(formUsername + " " + formPassword);
 
             int user = UserController.loginUser(formUsername, formPassword);
 
@@ -68,39 +102,75 @@ public class LandingPage extends BaseScene implements Showable {
             // loginUser(formUsername,formPassword);
         });
 
-        Button registerButton = new Button("Login");
+        Button registerButton = new Button("Register");
+        registerButton.setStyle(
+            "-fx-background-color: #58585B;" +
+            "-fx-border-color: none;" +
+            "-fx-background-radius: 10;"+
+            "-fx-font-family: 'Jaldi';" +
+            "-fx-font-size: 15px;" +
+            "-fx-font-weight: 400;" +
+            "-fx-line-height: 34px;" +
+            "-fx-text-fill: white;"
+        );
+        registerButton.setMinWidth(115);
+        registerButton.setMinHeight(45);
         registerButton.setOnAction(action -> {
             String formUsername = username.getText();
             String formPassword = password.getText();
 
-            int user = UserController.loginUser(formUsername, formPassword);
+            int user = UserController.registerUser(formUsername, formPassword);
 
             if (user != 0) {
-                MenuPage menuPageScene = new MenuPage(stage);
-                menuPageScene.show();
+                
             }
 
             // loginUser(formUsername,formPassword);
         });
 
         Button adminLoginButton = new Button("Login Admin");
+        adminLoginButton.setStyle(
+            "-fx-background-color: #58585B;" +
+            "-fx-border-color: none;" +
+            "-fx-background-radius: 10;"+
+            "-fx-font-family: 'Jaldi';" +
+            "-fx-font-size: 15px;" +
+            "-fx-font-weight: 400;" +
+            "-fx-line-height: 34px;" +
+            "-fx-text-fill: white;"
+        );
+        adminLoginButton.setMinWidth(250);
+        adminLoginButton.setMinHeight(45);
         adminLoginButton.setOnAction(action -> {
-            // TODO make function that check if user is success to login
-            // TODO redirect to managementPage() if success login;
+            String formUsername = username.getText();
+            String formPassword = password.getText();
 
+            int user = UserController.loginUserAdmin(formUsername, formPassword);
+
+            if (user != 0) {
+                MenuPage menuPageScene = new MenuPage(stage);
+                menuPageScene.show();
+            }
         });
 
-        HBox notLoginContainer = new HBox();
-        notLoginContainer.getChildren().addAll(adminLoginButton);
-        notLoginContainer.setAlignment(Pos.BASELINE_CENTER);
-        notLoginContainer.setSpacing(10);
-        // StackPane stackPane = new StackPane();
-        // stackPane.getChildren().add(brandLogo);
+        buttonContainer.add(loginButton, 0, 0);
+        buttonContainer.add(registerButton, 1, 0);
+        buttonContainer.add(adminLoginButton, 0, 1, 2, 1);
 
-        VBox layout = new VBox();
-        layout.getChildren().addAll(loginLabel, brandContainer, formContainer, loginButton, notLoginContainer);
+        buttonContainer.setAlignment(Pos.CENTER);
+        buttonContainer.setHgap(20);
+        buttonContainer.setVgap(10);
+
+        GridPane layout = new GridPane();
+        layout.add(headerContainer,0,0);
+        layout.add(logoContainer,0,1);
+        layout.add(formContainer,0,2);
+        layout.add(buttonContainer,0,3);
+        // layout.getChildren().addAll(loginLabel, brandContainer, formContainer, buttonContainer);
+        // layout.setAlignment(Pos.TOP_CENTER);
+        // layout.setSpacing(20);
         layout.setAlignment(Pos.TOP_CENTER);
-        layout.setSpacing(20);
+        layout.setVgap(20);
 
         Scene scene = new Scene(layout, stage.getWidth(), stage.getHeight());
 
