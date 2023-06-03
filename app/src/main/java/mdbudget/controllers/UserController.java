ad0f85406045f61d409a245a40fd67d335a897fd
NitoCoding <ilham.mks003@gmail.com>
2023-06-03 17:57:50 +0800

    feat: add getUserId and getAllDaataUser and little fix query adminlogin

--- app/src/main/java/mdbudget/controllers/UserController.java
+++ app/src/main/java/mdbudget/controllers/UserController.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import mdbudget.models.BaseModel;
+import mdbudget.models.User;
 import mdbudget.utils.Connector;
 import mdbudget.utils.IdDatabaseGenerator;
 
@@ -52,7 +55,7 @@ public class UserController {
         int id = 0;
         try {
             conn = Connector.getConnection();
-            String queryOrder = "SELECT * FROM user WHERE userNama = ? AND userPassword = ? AND userRole = 'admin' LIMIT 1";
+            String queryOrder = "SELECT * FROM tb_User WHERE userNama = ? AND userPassword = ? LIMIT 1";
             statement = conn.prepareStatement(queryOrder);
             statement.setString(1, nama);
             statement.setString(2, password);
@@ -119,5 +122,84 @@ public class UserController {
             }
         }
         return rowsAffected;
+    }
+
+    public static User getUserById(int id){
+        User user = null;
+        try {
+            conn = Connector.getConnection();
+            String queryOrder = "SELECT * FROM tb_User WHERE userId == ? LIMIT 1";
+            statement = conn.prepareStatement(queryOrder);
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                int userId = resultSet.getInt("userId");
+                String userNama = resultSet.getString("userNama");
+                String userPass = resultSet.getString("userPassword");
+                String userRole = resultSet.getString("userRole");
+
+                user = new User(userId, userNama, userPass, userRole);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the database connections
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                // Handle the exception or log the error
+                e.printStackTrace();
+            }
+        }
+        return user;
+    }
+
+    public static ArrayList<BaseModel> getAllDataUsers(){
+        ArrayList<BaseModel> users = new ArrayList<>();
+        try {
+            conn = Connector.getConnection();
+            String queryOrder = "SELECT * FROM tb_User";
+            statement = conn.prepareStatement(queryOrder);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int userId = resultSet.getInt("userId");
+                String userNama = resultSet.getString("userNama");
+                String userPass = resultSet.getString("userPassword");
+                String userRole = resultSet.getString("userRole");
+
+                User user = new User(userId, userNama, userPass, userRole);
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the database connections
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                // Handle the exception or log the error
+                e.printStackTrace();
+            }
+        }
+        return users;
     }
 }
